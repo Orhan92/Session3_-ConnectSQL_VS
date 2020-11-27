d6655c6fd0521bac4eebee50f08ae6110d772eec
Warchiefen <70097003+Warchiefen@users.noreply.github.com>
2020-11-27 14:59:10 +0100

    Add project files.

---
+++ Connect_SQL_With_VS_Console/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Add_Song_To_SQL_Console
+{
+
+    public class Program
+    {
+        private static SqlConnection connection;
+        public static void Main()
+        {
+
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            connection = new SqlConnection(@"Data Source=(local)\SQLExpress;Initial Catalog=MusicDataBase;Integrated Security=SSPI;");
+            connection.Open();
+
+            Console.WriteLine("Add a new song!");
+
+            Console.Write("Title: ");
+            string insertTitle = Console.ReadLine();
+
+            Console.Write("Length: ");
+            int insertLength = int.Parse(Console.ReadLine());
+
+            Console.Write("Does your song have a music video [yes/no]: ");
+            string insertVideo = Console.ReadLine();
+            if (insertVideo == "yes" || insertVideo == "y")
+            {
+                insertVideo = true.ToString();
+            }
+            else
+            {
+                insertVideo = false.ToString();
+            }
+
+            Console.Write("Lyrics (if you have, otherwise press enter): ");
+            string insertLyrics = Console.ReadLine();
+            if (insertLyrics == string.Empty)
+            {
+                insertLyrics = "NULL";
+            }
+
+
+            string insertSQL = "INSERT INTO Song (Title, Length, HasMusicVideo, Lyrics) VALUES (@Title, @Length, @HasMusicVideo, @Lyrics)";
+            SqlCommand insertCommand = new SqlCommand(insertSQL, connection);
+            insertCommand.Parameters.Add(new SqlParameter
+            {
+                ParameterName = "@Title",
+                Value = insertTitle
+            });
+            insertCommand.Parameters.Add(new SqlParameter
+            {
+                ParameterName = "@Length",
+                Value = insertLength
+            });
+            insertCommand.Parameters.Add(new SqlParameter
+            {
+                ParameterName = "@HasMusicVideo",
+                Value = insertVideo
+            });
+            insertCommand.Parameters.Add(new SqlParameter
+            {
+                ParameterName = "@Lyrics",
+                Value = insertLyrics
+            });
+
+            insertCommand.ExecuteNonQuery();
+
+            Console.WriteLine("\nSong archive:\n");
+            ReadSQLlistFromSong();
+        }
+
+        public static void ReadSQLlistFromSong()
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM Song", connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string title = Convert.ToString(reader["Title"]);
+                int totalMinutes = Convert.ToInt32(reader["Length"]);
+                double totalSeconds = Convert.ToDouble(reader["Length"]);
+                int minutes = totalMinutes / 60;
+                string seconds = (totalSeconds % 60).ToString();
+
+                if (seconds.Length < 2)
+                {
+                    seconds = "0" + seconds;
+                }
+
+                string totalTime = minutes.ToString() + ":" + seconds.ToString();
+
+                Console.WriteLine(title + ": " + totalTime);
+            }
+        }
+    }
+}
---
+++ Session3_ ConnectSQL_VS/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session3__ConnectSQL_VS
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            SqlConnection connection = new SqlConnection(@"Data Source=(local)\SQLExpress;Initial Catalog=MusicDataBase;Integrated Security=SSPI;");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Song", connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            Console.WriteLine("Song archive");
+            // Denna loop fortsätter tills alla rader har lästs (genom att Read() returnerar en boolean).
+            while (reader.Read())
+            {
+                string title = Convert.ToString(reader["Title"]);
+                int totalMinutes = Convert.ToInt32(reader["Length"]);
+                double totalSeconds = Convert.ToDouble(reader["Length"]);
+                int minutes = totalMinutes / 60;
+                string seconds = (totalSeconds % 60).ToString();
+
+                if(seconds.Length < 2)
+                {
+                    seconds = "0" + seconds;
+                }
+
+                string totalTime = minutes.ToString() + ":" + seconds.ToString();
+
+                Console.WriteLine(title + ": " + totalTime);
+            }
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            using FakeConsole console = new FakeConsole("First input", "Second input");
+            Program.Main();
+            Assert.AreEqual("Hello!", console.Output);
+        }
+    }
+}
